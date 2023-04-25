bb13d2e8c70e0389ee1349ab3632e749e077e8b1
Aytac <aytacibrahimova2612@gmail.com>
2023-04-25 22:52:09 +0400

    last 1

--- components/MapNew.js
+++ components/MapNew.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState, useEffect } from 'react';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
-import { StyleSheet, View, Dimensions, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Dimensions, Text, TouchableOpacity, Image} from 'react-native';
 import { GooglePlacesAutocomplete, GooglePlaceDetail } from 'react-native-google-places-autocomplete';
 import Constants from 'expo-constants';
 import MapViewDirections from 'react-native-maps-directions';
@@ -8,6 +8,7 @@ import * as Location from 'expo-location';
 import axios from 'axios';
 import { io } from 'socket.io-client';
 import { clearLogEntriesAsync } from 'expo-updates';
+import { EngineIcon } from '../assets/icons/icon';
 
 // import {GOOGLE_API_KEY} from '@env'
 
@@ -25,25 +26,24 @@ const INITIAL_POSITION = {
 }
 
 export default function Map() {
+  const [permissionStatus, setPermissionStatus] = useState()
   const [origin, setOrigin] = useState()
-  const [destination, setDestination] = useState()
-
+  const [destination, setDestination] = useState(
+  //   {
+  //   latitude: 40.226650,
+  //   longitude: 49.575770
+  // }
+  )
   const [showDirections, setShowDirections] = useState(false)
-  const [currentLocation, setCurrentLocation] = useState(null)
-
-  // console.log(currentLocation);
-  const mapRef = useRef()
-
-  const [openInput, setOpenInput] = useState(true)
-
-  const moveTo = async (position) => {
-    const camera = await mapRef.current?.getCamera()
-    if(camera) {
-      camera.center = position;
-      mapRef.current.animateCamera(camera, {duration: 10})
+  const [initialState, setInitialState] = useState({
+    region: {
+      latitude: '',
+      longitude: '',
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
     }
-  }
-
+  })
+  const mapRef = useRef()
   const edgePaddingValue = 70
   const edgePadding = {
     top: edgePaddingValue,
@@ -61,88 +61,109 @@ export default function Map() {
     }
   }
 
+  const moveTo = async (position) => {
+    const camera = await mapRef.current?.getCamera()
+    if(camera) {
+      camera.center = position;
+      mapRef.current.animateCamera(camera, {duration: 10})
+    }
+  }
+
   const traceRoute = () => {
     if(origin && destination) {
       setShowDirections(true)
       mapRef.current.fitToCoordinates([origin, destination], {edgePadding})
     }
-    setOpenInput(false)
   }
-
-  const [initialState, setInitialState] = useState({
-    region: {
-      latitude: currentLocation ? currentLocation.coords.latitue : null,
-      longitude: currentLocation ? currentLocation.coords.longitude : null,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    }
-  })
 
   const sendData = () => {
     socket.emit("location", origin)
   }
 
-  const getData = () => {
-    socket.on("address", (data) => {
-      setDestination( data ?
+  // 3 step
+  const getData = async () => {
+    console.log('get data step');
+    const data = await socket.on("address", (data) => {
+      setDestination(
+        data ?
         {
         latitude: data.lat,
         longitude: data.lng
-      }
-      : null
-      )
+        } : null )
     })
   }
 
+  // 1 step
   const getPermissions = async () => {
-    let {status} = await Location.requestForegroundPermissionsAsync();
-    if(status !== 'granted') {
-      return; 
-    }
-    let currentLocation = await Location.getCurrentPositionAsync({});
-    setCurrentLocation(currentLocation)
-    setOrigin(
-      currentLocation ?
-      {
-      latitude: currentLocation.coords.latitude,
-      longitude: currentLocation.coords.longitude
-    }
-    : null
-    )
-    moveTo({
-      latitude: currentLocation ? currentLocation.coords.latitude : null,
-      longitude: currentLocation ? currentLocation.coords.longitude : null
-    })
+      let {status} = await Location.requestForegroundPermissionsAsync();
+      if(status !== 'granted') {
+        return; 
+      }
+      setPermissionStatus(status)
+      getCurrentLoc()
+  }
+  const getCurrentLoc = async () => {
+    let currentLoc = await Location.getCurrentPositionAsync({});
+    setOrigin({
+      latitude: currentLoc.coords.latitude,
+      longitude: currentLoc.coords.longitude
+      })
     setInitialState({
       region: {
-        latitude: currentLocation ? currentLocation.coords.latitude : null,
-        longitude: currentLocation ? currentLocation.coords.longitude : null,
+        latitude: currentLoc.coords.latitude,
+        longitude: currentLoc.coords.longitude,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       }
     })
-    
-  };
+    setTimeout(() => {
+        setInterval(() => {
+          // console.log('interval');
+          if(origin) {
+            sendData()
+            getOrigin()
+          } else {
+            return;
+          }
+        }, 5000)
+      }, 5000)
+  }
+  // 2 step
+  const getOrigin = async  () => {
+
+    if(permissionStatus === 'granted') {
+      let currentLoc = await Location.getCurrentPositionAsync({})
+      setOrigin({
+        latitude: currentLoc.coords.latitude,
+        longitude: currentLoc.coords.longitude
+        })
+    } else {
+      return;
+    }
+  }
+
 
   useEffect(() => {
-    getPermissions();
-    // getData()
+    if( permissionStatus !== 'granted') {
+      getPermissions();
+    } else {
+      getCurrentLoc()
+    }
   },[])
 
+
+  // 5 step
   useEffect(() => {
+    console.log('no dest');
     if(destination) {
+      console.log('yes dest');
+    console.log(destination);
+
       traceRoute()
-      console.log('lat useffect');
-      console.log(destination);
     }
   }, [destination])
 
-  useEffect(() => {
-      setInterval(() => {
-        sendData()
-        // console.log('send');
-      }, 10000)
-  }, [origin])
+ 
 
 
   return (
@@ -154,7 +175,10 @@ export default function Map() {
         region={initialState.region}
         // onRegionChange={() => setInitialState()}
       >
-        {origin && <Marker coordinate={origin} />}
+        {origin && <Marker coordinate={origin} pinColor='green'>
+          <View></View>
+          <EngineIcon />            
+        </Marker> }
         {destination && <Marker coordinate={destination} />}
         {showDirections && origin && destination ?  <MapViewDirections
           origin={origin}
@@ -162,15 +186,19 @@ export default function Map() {
           apikey={'AIzaSyDFave-hQPrKugZTnAQ-lXf7ejukC0JPNw'}
           strokeColor="#6644ff"
           strokeWidth={4}
-          onReady={traceRouteOnReady}
+          // onReady={traceRouteOnReady}
           />
           :
         null}
       </MapView>
-
-        <TouchableOpacity style={styles.button} onPress={traceRoute}>
+        {
+          origin && destination ? 
+          <TouchableOpacity style={styles.button} onPress={traceRoute}>
             <Text style={styles.buttonText}>Trace route</Text>
-        </TouchableOpacity>
+          </TouchableOpacity>
+          : null
+        }
+      
     </View>
   );
 }
@@ -202,5 +230,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#90be6d', 
     padding: 5,
     borderRadius: 4,
-  }
+  },
 });
